8e2c63b519454b7867d3bc69ebadf0c3bea46a9e
Christian Muehlhaeuser <muesli@gmail.com>
2014-04-08 08:46:26 +0200

    * Fixed saturation more and go fmt'd.

--- crop.go
+++ crop.go
@@ -1,59 +1,61 @@
 package main
 
-import(
+import (
 	"errors"
+	"fmt"
 	"image"
 	"image/color"
 	"image/jpeg"
 	_ "image/png"
-	"fmt"
 	"math"
 	"os"
+	"time"
 )
 
-var(
-    aspect = 0
-    cropWidth = 0.0
-    cropHeight = 0.0
-    detailWeight = 0.2
-    skinColor = [3]float64{0.78, 0.57, 0.44}
-    skinBias = 0.01
-    skinBrightnessMin = 0.2
-    skinBrightnessMax = 1.0
-    skinThreshold = 0.8
-    skinWeight = 1.8
-    saturationBrightnessMin = 0.25
-    saturationBrightnessMax = 0.9
-    saturationThreshold = 0.4
-    saturationBias = 0.2
-    saturationWeight = 0.3
-    // step * minscale rounded down to the next power of two should be good
-    scoreDownSample = 8
-    step = 8
-    scaleStep = 0.1
-    minScale = 0.9
-    maxScale = 1.0
-    edgeRadius = 0.4
-    edgeWeight = -20.0
-    outsideImportance = -0.5
-    ruleOfThirds = true
-    prescale = true
-    debug = false
+var (
+	aspect                  = 0
+	cropWidth               = 0.0
+	cropHeight              = 0.0
+	detailWeight            = 0.2
+	skinColor               = [3]float64{0.78, 0.57, 0.44}
+	skinBias                = 0.01
+	skinBrightnessMin       = 0.2
+	skinBrightnessMax       = 1.0
+	skinThreshold           = 0.8
+	skinWeight              = 1.8
+	saturationBrightnessMin = 0.25
+	saturationBrightnessMax = 0.9
+	saturationThreshold     = 0.4
+	saturationBias          = 0.2
+	saturationWeight        = 0.3
+	// step * minscale rounded down to the next power of two should be good
+	scoreDownSample   = 8
+	invDownSample     = 1.0 / float64(scoreDownSample)
+	step              = 8
+	scaleStep         = 0.1
+	minScale          = 0.9
+	maxScale          = 1.0
+	edgeRadius        = 0.4
+	edgeWeight        = -20.0
+	outsideImportance = -0.5
+	ruleOfThirds      = true
+	prescale          = true
+	debug             = false
 )
 
 type Score struct {
-	Detail float64
+	Detail     float64
 	Saturation float64
-	Skin float64
-	Total float64
+	Skin       float64
+	Total      float64
 }
 
 type Crop struct {
-	X int
-	Y int
-	Width int
+	X      int
+	Y      int
+	Width  int
 	Height int
-	Score Score
+	Score  Score
 }
 
 type SubImager interface {
@@ -61,27 +63,27 @@ type SubImager interface {
 }
 
 func thirds(x float64) float64 {
-	x1 := int(x-(1.0/3.0) + 1.0)
-	res := (float64(x1 % 2.0) * 0.5) - 0.5
+	x1 := int(x - (1.0 / 3.0) + 1.0)
+	res := (float64(x1%2.0) * 0.5) - 0.5
 	return res * 16.0
 }
 
-func importance(crop Crop, x, y int) float64 {
-	if crop.X > x || x >= crop.X + crop.Width || crop.Y > y || y >= crop.Y + crop.Height {
+func importance(crop *Crop, x, y int) float64 {
+	if crop.X > x || x >= crop.X+crop.Width || crop.Y > y || y >= crop.Y+crop.Height {
 		return outsideImportance
 	}
 
-	xf := float64(x - crop.X) / float64(crop.Width)
-	yf := float64(y - crop.Y) / float64(crop.Height)
+	xf := float64(x-crop.X) / float64(crop.Width)
+	yf := float64(y-crop.Y) / float64(crop.Height)
 
-	px := math.Abs(0.5 - xf) * 2.0
-	py := math.Abs(0.5 - yf) * 2.0
+	px := math.Abs(0.5-xf) * 2.0
+	py := math.Abs(0.5-yf) * 2.0
 
-	dx := math.Max(px - 1.0 + edgeRadius, 0.0)
-	dy := math.Max(py - 1.0 + edgeRadius, 0.0)
-	d := (dx*dx + dy*dy) * edgeWeight
+	dx := math.Max(px-1.0+edgeRadius, 0.0)
+	dy := math.Max(py-1.0+edgeRadius, 0.0)
+	d := (math.Pow(dx, 2) + math.Pow(dy, 2)) * edgeWeight
 
-	s := 1.41 - math.Sqrt(px*px + py*py)
+	s := 1.41 - math.Sqrt(math.Pow(px, 2)+math.Pow(py, 2))
 	if ruleOfThirds {
 		s += (math.Max(0.0, s+d+0.5) * 1.2) * (thirds(px) + thirds(py))
 	}
@@ -89,37 +91,32 @@ func importance(crop Crop, x, y int) float64 {
 	return s + d
 }
 
-func score(output *image.Image, crop Crop) Score {
-	invDownSample := 1.0 / float64(scoreDownSample)
+func score(output *image.Image, crop *Crop) Score {
 	height := (*output).Bounds().Size().Y
 	width := (*output).Bounds().Size().X
 	score := Score{}
 
-	for y := 0; y < height * scoreDownSample; y += scoreDownSample {
-		for x := 0; x < width * scoreDownSample; x += scoreDownSample {
-			p := int(math.Floor(float64(y) * invDownSample)) * width + int(math.Floor(float64(x) * invDownSample))
+	for y := 0; y < height*scoreDownSample; y += scoreDownSample {
+		for x := 0; x < width*scoreDownSample; x += scoreDownSample {
+			//			now := time.Now()
 			imp := importance(crop, x, y)
+			//			fmt.Println("Time elapsed single-imp:", time.Since(now))
 
-			ny := int(math.Floor(float64(p) / float64(width)))
-			nx := 0
-			if ny > 0 {
-				nx = p % width
-			} else {
-				nx = p
-			}
+			ny := int(float64(y) * invDownSample)
+			nx := int(float64(x) * invDownSample)
 
 			r, g, b, _ := (*output).At(nx, ny).RGBA()
-			r8 := float64(r>>8)
-			g8 := float64(g>>8)
-			b8 := float64(b>>8)
+			r8 := float64(r >> 8)
+			g8 := float64(g >> 8)
+			b8 := float64(b >> 8)
 
-			score.Skin += (r8/255.0) * (g8/255.0 + skinBias) * imp
-			score.Detail += (g8/255.0) * imp
-			score.Saturation += (b8/255.0) * (g8/255.0 + saturationBias) * imp
+			score.Skin += (r8 / 255.0) * (g8/255.0 + skinBias) * imp
+			score.Detail += (g8 / 255.0) * imp
+			score.Saturation += (b8 / 255.0) * (g8/255.0 + saturationBias) * imp
 		}
 	}
 
-	score.Total = (score.Detail * detailWeight + score.Skin * skinWeight + score.Saturation * saturationWeight) / float64(crop.Width) / float64(crop.Height)
+	score.Total = (score.Detail*detailWeight + score.Skin*skinWeight + score.Saturation*saturationWeight) / float64(crop.Width) / float64(crop.Height)
 	return score
 }
 
@@ -137,26 +134,39 @@ func writeImage(img *image.Image, name string) {
 func analyse(img image.Image) Crop {
 	o := image.Image(image.NewRGBA(img.Bounds()))
 
+	now := time.Now()
 	edgeDetect(&img, &o)
+	fmt.Println("Time elapsed edge:", time.Since(now))
 	writeImage(&o, "/tmp/foo_step1.jpg")
+
+	now = time.Now()
 	skinDetect(&img, &o)
+	fmt.Println("Time elapsed skin:", time.Since(now))
 	writeImage(&o, "/tmp/foo_step2.jpg")
+
+	now = time.Now()
 	saturationDetect(&img, &o)
+	fmt.Println("Time elapsed sat:", time.Since(now))
 	writeImage(&o, "/tmp/foo_step3.jpg")
 
+	now = time.Now()
 	var topCrop Crop
 	topScore := -1.0
 	cs := crops(&o)
+	fmt.Println("Time elapsed crops:", time.Since(now), len(cs))
 
+	now = time.Now()
 	for _, crop := range cs {
-		crop.Score = score(&o, crop)
+		//		nowIn := time.Now()
+		crop.Score = score(&o, &crop)
+		//		fmt.Println("Time elapsed single-score:", time.Since(nowIn))
 		if crop.Score.Total > topScore {
 			topCrop = crop
 			topScore = crop.Score.Total
 		}
 	}
-
 	fmt.Printf("Top crop: %+v\n", topCrop)
+	fmt.Println("Time elapsed score:", time.Since(now))
 
 	cropImage := img.(SubImager).SubImage(image.Rect(topCrop.X, topCrop.Y, topCrop.Width+topCrop.X, topCrop.Height+topCrop.Y))
 	writeImage(&cropImage, "/tmp/foo_topcrop.jpg")
@@ -169,59 +179,64 @@ func crop(img image.Image, width, height int) error {
 		return errors.New("Expect either a height or width")
 	}
 
-	scale := math.Min(float64(img.Bounds().Size().X) / float64(width), float64(img.Bounds().Size().Y) / float64(height))
-	cropWidth, cropHeight = math.Floor(float64(width) * scale), math.Floor(float64(height) * scale)
-	minScale = math.Min(maxScale, math.Max(1.0 / scale, minScale))
+	scale := math.Min(float64(img.Bounds().Size().X)/float64(width), float64(img.Bounds().Size().Y)/float64(height))
+	cropWidth, cropHeight = math.Floor(float64(width)*scale), math.Floor(float64(height)*scale)
+	minScale = math.Min(maxScale, math.Max(1.0/scale, minScale))
 
 	fmt.Printf("original resolution: %dx%d\n", img.Bounds().Size().X, img.Bounds().Size().Y)
 	fmt.Printf("scale: %f, cropw: %f, croph: %f, minscale: %f\n", scale, cropWidth, cropHeight, minScale)
 
+	now := time.Now()
 	analyse(img)
+	fmt.Println("Time elapsed:", time.Since(now))
 
 	return nil
 }
 
 func saturation(c color.Color) float64 {
 	r, g, b, _ := c.RGBA()
+	r8 := float64(r >> 8)
+	g8 := float64(g >> 8)
+	b8 := float64(b >> 8)
 
-	maximum := math.Max(math.Max(float64(r)/255, float64(g)/255), float64(b)/255)
-	minimum := math.Min(math.Min(float64(r)/255, float64(g)/255), float64(b)/255)
+	maximum := math.Max(math.Max(r8/255.0, g8/255.0), b8/255.0)
+	minimum := math.Min(math.Min(r8/255.0, g8/255.0), b8/255.0)
 
-	if (maximum == minimum) {
+	if maximum == minimum {
 		return 0
 	}
 
-	l := (maximum + minimum) / 2
+	l := (maximum + minimum) / 2.0
 	d := maximum - minimum
 
 	if l > 0.5 {
-		return d / (2-maximum-minimum)
+		return d / (2.0 - maximum - minimum)
 	} else {
-		return d / (maximum+minimum)
+		return d / (maximum + minimum)
 	}
 }
 
 func cie(c color.Color) float64 {
 	r, g, b, _ := c.RGBA()
-	r8 := float64(r>>8)
-	g8 := float64(g>>8)
-	b8 := float64(b>>8)
+	r8 := float64(r >> 8)
+	g8 := float64(g >> 8)
+	b8 := float64(b >> 8)
 
 	return 0.5126*b8 + 0.7152*g8 + 0.0722*r8
 }
 
 func skinCol(c color.Color) float64 {
 	r, g, b, _ := c.RGBA()
-	r8 := float64(r>>8)
-	g8 := float64(g>>8)
-	b8 := float64(b>>8)
+	r8 := float64(r >> 8)
+	g8 := float64(g >> 8)
+	b8 := float64(b >> 8)
 
-	mag := math.Sqrt(r8*r8 + g8*g8 + b8*b8)
+	mag := math.Sqrt(math.Pow(r8, 2) + math.Pow(g8, 2) + math.Pow(b8, 2))
 	rd := r8/mag - skinColor[0]
 	gd := g8/mag - skinColor[1]
 	bd := b8/mag - skinColor[2]
 
-	d := math.Sqrt(rd*rd + gd*gd + bd*bd)
+	d := math.Sqrt(math.Pow(rd, 2) + math.Pow(gd, 2) + math.Pow(bd, 2))
 	return 1.0 - d
 }
 
@@ -236,11 +251,11 @@ func edgeDetect(i *image.Image, o *image.Image) {
 			if x == 0 || x >= w-1 || y == 0 || y >= h-1 {
 				lightness = cie((*i).At(x, y))
 			} else {
-				lightness = cie((*i).At(x, y)) * 4.0 -
-							cie((*i).At(x, y-1)) -
-							cie((*i).At(x-1, y)) -
-							cie((*i).At(x+1, y)) -
-							cie((*i).At(x, y+1))
+				lightness = cie((*i).At(x, y))*4.0 -
+					cie((*i).At(x, y-1)) -
+					cie((*i).At(x-1, y)) -
+					cie((*i).At(x+1, y)) -
+					cie((*i).At(x, y+1))
 			}
 
 			if lightness < 0 {
@@ -263,13 +278,13 @@ func skinDetect(i *image.Image, o *image.Image) {
 			skin := skinCol((*i).At(x, y))
 
 			if skin > skinThreshold && lightness >= skinBrightnessMin && lightness <= skinBrightnessMax {
-				r := (skin - skinThreshold) * (255.0/(1.0 - skinThreshold))
+				r := (skin - skinThreshold) * (255.0 / (1.0 - skinThreshold))
 				_, g, b, _ := (*o).At(x, y).RGBA()
-				nc := color.RGBA{uint8(r), uint8(g>>8), uint8(b>>8), 255}
+				nc := color.RGBA{uint8(r), uint8(g >> 8), uint8(b >> 8), 255}
 				(*o).(*image.RGBA).Set(x, y, nc)
 			} else {
 				_, g, b, _ := (*o).At(x, y).RGBA()
-				nc := color.RGBA{0, uint8(g>>8), uint8(b>>8), 255}
+				nc := color.RGBA{0, uint8(g >> 8), uint8(b >> 8), 255}
 				(*o).(*image.RGBA).Set(x, y, nc)
 			}
 		}
@@ -286,13 +301,13 @@ func saturationDetect(i *image.Image, o *image.Image) {
 			saturation := saturation((*i).At(x, y))
 
 			if saturation > saturationThreshold && lightness >= saturationBrightnessMin && lightness <= saturationBrightnessMax {
-				b := (saturation - saturationThreshold) * (255.0/(1.0 - saturationThreshold))
+				b := (saturation - saturationThreshold) * (255.0 / (1.0 - saturationThreshold))
 				r, g, _, _ := (*o).At(x, y).RGBA()
-				nc := color.RGBA{uint8(r>>8), uint8(g>>8), uint8(b), 255}
+				nc := color.RGBA{uint8(r >> 8), uint8(g >> 8), uint8(b), 255}
 				(*o).(*image.RGBA).Set(x, y, nc)
 			} else {
 				r, g, _, _ := (*o).At(x, y).RGBA()
-				nc := color.RGBA{uint8(r>>8), uint8(g>>8), 0, 255}
+				nc := color.RGBA{uint8(r >> 8), uint8(g >> 8), 0, 255}
 				(*o).(*image.RGBA).Set(x, y, nc)
 			}
 		}
@@ -305,17 +320,17 @@ func crops(i *image.Image) []Crop {
 	height := (*i).Bounds().Size().Y
 
 	//minDimension := math.Min(float64(width), float64(height))
-	cropW := cropWidth //|| minDimension
+	cropW := cropWidth  //|| minDimension
 	cropH := cropHeight //|| minDimension
 
 	for scale := maxScale; scale >= minScale; scale -= scaleStep {
 		for y := 0; float64(y)+cropH*scale <= float64(height); y += step {
 			for x := 0; float64(x)+cropW*scale <= float64(width); x += step {
 				res = append(res, Crop{
-					X: x,
-					Y: y,
-					Width: int(cropW*scale),
-					Height: int(cropH*scale),
+					X:      x,
+					Y:      y,
+					Width:  int(cropW * scale),
+					Height: int(cropH * scale),
 				})
 			}
 		}
