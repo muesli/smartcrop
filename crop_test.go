78668f03246e346aa35ce0a66c3d78a6549c89d7
Michael Wendland <accounts@michiwend.com>
2014-09-10 17:52:06 +0200

    adds benchmark to testcrop a whole directory

--- crop_test.go
+++ crop_test.go
@@ -6,7 +6,9 @@ import (
 	"image"
 	_ "image/jpeg"
 	_ "image/png"
+	"io/ioutil"
 	"os"
+	"strings"
 	"testing"
 )
 
@@ -43,4 +45,44 @@ func TestCrop(t *testing.T) {
 	}
 
 }
+
+func BenchmarkImageDir(b *testing.B) {
+
+	b.SetParallelism(4)
+	b.StopTimer()
+
+	files, err := ioutil.ReadDir("./samples")
+	if err != nil {
+		b.Error(err)
+	}
+
+	b.StartTimer()
+	for _, file := range files {
+		if strings.Contains(file.Name(), ".jpg") {
+
+			fi, _ := os.Open("./samples/" + file.Name())
+			defer fi.Close()
+
+			img, _, err := image.Decode(fi)
+			if err != nil {
+				b.Error(err)
+			}
+
+			topCrop, scaledImg, err := SmartCrop(&img, 300, 300)
+			if err != nil {
+				b.Error(err)
+			}
+
+			sub, ok := scaledImg.(SubImager)
+			if ok {
+				cropImage := sub.SubImage(image.Rect(topCrop.X, topCrop.Y, topCrop.Width+topCrop.X, topCrop.Height+topCrop.Y))
+				WriteImageToJpeg(&cropImage, "/tmp/smartcrop/smartcrop-"+file.Name())
+			} else {
+				b.Error(errors.New("No SubImage support"))
+			}
+		}
+	}
+
+	//fmt.Println("average time/image:", b.t
+
 }
