3eda655fc3b1c37a120b332f857030dd67e0dce5
Michael Wendland <accounts@michiwend.com>
2014-10-14 00:25:39 +0200

    using hatchet examples

--- crop_test.go
+++ crop_test.go
@@ -74,10 +74,9 @@ func TestCrop(t *testing.T) {
 
 func BenchmarkImageDir(b *testing.B) {
 
-	b.SetParallelism(4)
 	b.StopTimer()
 
-	files, err := ioutil.ReadDir("./samples")
+	files, err := ioutil.ReadDir("./samples/hatchet")
 	if err != nil {
 		b.Error(err)
 	}
@@ -86,7 +85,7 @@ func BenchmarkImageDir(b *testing.B) {
 	for _, file := range files {
 		if strings.Contains(file.Name(), ".jpg") {
 
-			fi, _ := os.Open("./samples/" + file.Name())
+			fi, _ := os.Open("./samples/hatchet/" + file.Name())
 			defer fi.Close()
 
 			img, _, err := image.Decode(fi)
